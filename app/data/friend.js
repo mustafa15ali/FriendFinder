67ff087faae455a5d1cbbbc4c412d77d5d7a4926
Mustafa Farah <mustafa.farah@outlook.com>
2018-05-14 17:52:51 -0500

    added fixes

---
+++ app/data/friend.js
@@ -0,0 +1,103 @@
+// Data for all possible friends.
+
+let friends = [{
+        name: "Bob the builder",
+        photo: "https://news.nationalgeographic.com/content/dam/news/2016/02/24/matthew_henson/08_matthew_henson.jpg",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Parker",
+        photo: "https://avatars3.githubusercontent.com/u/11880656?s=460&v=4",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Kyle AKA Eric",
+        photo: "http://ethnicelebs.com/wp-content/uploads/2017/08/LSA-004652Eric-Andre-400x600.jpg",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Moose the Duece",
+        photo: "https://avatars0.githubusercontent.com/u/32945168?s=400&u=5dfffffad2ce35d5ce26ce32b4a60f0ac95f0ca4&v=4",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Billy Jean",
+        photo: "https://i.pinimg.com/originals/a3/62/c6/a362c6bd08e426f973ac057ab565b08b.jpg",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "New Bae",
+        photo: "https://i.ytimg.com/vi/xjHD_LCI33Q/maxresdefault.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    }
+
+];
+
+//   Exporting the array. 
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,52 @@
+var friends = require("../data/friends");
+
+// ROUTING
+
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        var bestFriend = {
+            name: "",
+            photo: "",
+            friendDifference: Infinity
+        };
+
+        // Taking the user"s survey POST and parse it.
+        var userData = req.body;
+        var userScores = userData.scores;
+
+        var totalDifference;
+
+        for (var i = 0; i < friends.length; i++) {
+            var currentFriend = friends[i];
+            totalDifference = 0;
+
+            console.log(currentFriend.name);
+
+            for (var j = 0; j < currentFriend.scores.length; j++) {
+                var currentFriendScore = currentFriend.scores[j];
+                var currentUserScore = userScores[j];
+
+                //Calculate the difference between the scores and add them into the totalDifference
+                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+            }
+
+            if (totalDifference <= bestFriend.friendDifference) {
+
+                // Reset the bestFriend to be the new bestfriend.
+
+                bestFriend.name = currentFriend.name;
+                bestFriend.photo = currentFriend.photo;
+                bestFriend.friendDifference = totalDifference;
+            }
+        }
+
+        // Save the user's data to the database
+        friends.push(userData);
+
+        res.json(bestFriend);
+    });
+};
