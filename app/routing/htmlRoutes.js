faad72d930000e7bf7a18e1d28fc215e22b01d8b
Mustafa Farah <mustafa.farah@outlook.com>
2018-05-14 18:39:34 -0500

    still having file path issues

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,12 +2,12 @@ module.exports = function (app) {
 
     // HTML GET Requests
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
     // If no matching route is found default to home
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "/../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
 };
